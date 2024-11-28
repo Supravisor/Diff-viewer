598f792250bbaaf0aed9b0f68be8048c3bab61a6
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-11-29 09:28:48 +1300

    add button funtionality

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 
 const compare = document.getElementById("#div-compare");
 const original = document.getElementById("#div-original");
+
+const originalHide = document.getElementById("#original-button");
+const compareHide = document.getElementById("#compare-button");
+
+function original() {
+
+  if (originalHide.innerText === "Hide Original") {
+    original.style.display = "none";
+    originalHide.innerText = "Show Original";
+       }
+
+  else {
+    originalHide.innerText = "Hide Original";
+    original.style.display = "unset";
+       }
+
+}
